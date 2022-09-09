87184e32fe1519f8d7e2f0ab7b985efbc74dabb2
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-09-09 13:17:21 +0300

    add Linkedin

--- tiktok/urls.py
+++ tiktok/urls.py
@@ -1,9 +1,10 @@
 from django.db import router
 from django.urls import path, include
 
-from .views import AuthViews, CallBackViews
+from .views import AuthViews, CallBackViews, LinkedInCallBackView
 
 urlpatterns = [
     path('auth/', AuthViews.as_view()),
-    path('auth-callback/', CallBackViews.as_view())
+    path('auth-callback/', CallBackViews.as_view()),
+    path('signin-linkedin/', LinkedInCallBackView.as_view()),
 ]
